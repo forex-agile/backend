aa72167eca1efb5534a72e0a8d30d3081721d884
Dexter Lui <dexlui@pm.me>
2024-09-04 00:36:36 +0800

    feat: add AuthUser class for user authentication
    It implements UserDetails and has a User

---
+++ src/main/java/com/fdmgroup/forex/security/AuthUser.java
@@ -0,0 +1,56 @@
+package com.fdmgroup.forex.security;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fdmgroup.forex.models.User;
+
+public class AuthUser implements UserDetails {
+	private User user;
+
+	public AuthUser(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		System.out.println(new SimpleGrantedAuthority(this.user.getRole().toString()));
+		return Arrays.asList(new SimpleGrantedAuthority(this.user.getRole().toString()));
+	}
+
+	@Override
+	public String getPassword() {
+		return this.user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
