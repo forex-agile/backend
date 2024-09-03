e83569a83099bbe2c2cef269eb9e4b4767e2ed4b
Dexter Lui <dexlui@pm.me>
2024-09-04 00:59:09 +0800

    feat: add auth related annotations in main class

--- src/main/java/com/fdmgroup/forex/ForexApplication.java
+++ src/main/java/com/fdmgroup/forex/ForexApplication.java
@@ -2,8 +2,14 @@ package com.fdmgroup.forex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableWebSecurity
+@EnableMethodSecurity
 public class ForexApplication {
 
 	public static void main(String[] args) {
