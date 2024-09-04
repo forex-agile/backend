8b860ddda36a40188451c158094d04bdca4860e9
Dexter Lui <dexlui@pm.me>
2024-09-04 17:42:47 +0800

    feat(UserService): create a new user

--- src/main/java/com/fdmgroup/forex/services/UserService.java
+++ src/main/java/com/fdmgroup/forex/services/UserService.java
@@ -1,26 +1,39 @@
 package com.fdmgroup.forex.services;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.fdmgroup.forex.exceptions.InternalServerError;
+import com.fdmgroup.forex.models.Role;
 import com.fdmgroup.forex.models.User;
+import com.fdmgroup.forex.repos.RoleRepo;
 import com.fdmgroup.forex.repos.UserRepo;
 
+import jakarta.transaction.Transactional;
+
 /**
- * Service class for {@link com.fdmgroup.forex.models.User User} entity
+ * Provide services related to user
  */
+@Service
 public class UserService {
 
 	private UserRepo userRepo;
+	private RoleRepo roleRepo;
 	private PasswordEncoder pwdEncoder;
 
-	public UserService(UserRepo userRepo, PasswordEncoder pwdEncoder) {
+	public UserService(UserRepo userRepo, RoleRepo roleRepo, PasswordEncoder pwdEncoder) {
 		this.userRepo = userRepo;
+		this.roleRepo = roleRepo;
 		this.pwdEncoder = pwdEncoder;
 	}
 
-	public void createUser(User user) {
+	@Transactional
+	public User createUser(User user) throws InternalServerError {
+		Role role = roleRepo.findByRole("USER").orElseThrow(
+				() -> new InternalServerError("Internal Server Error: could not assign role"));
 		user.setPassword(pwdEncoder.encode(user.getPassword()));
-		userRepo.save(user);
+		user.setRole(role);
+		return userRepo.save(user);
 	}
 
 }
