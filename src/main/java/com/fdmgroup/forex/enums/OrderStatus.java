a687131dfe403beabca4640ce17d97442725c208
essteer <essteer@pm.me>
2024-09-06 19:43:35 +0800

    docs: Update javadocs for OrderStatus CANCELLED condition

--- src/main/java/com/fdmgroup/forex/enums/OrderStatus.java
+++ src/main/java/com/fdmgroup/forex/enums/OrderStatus.java
@@ -3,7 +3,7 @@ package com.fdmgroup.forex.enums;
 /**
  * Denotes the state of the order as follows:
  * ACTIVE: Order is live and open to matches
- * CANCELLED: Order cancelled by the user
+ * CANCELLED: Order cancelled by the user or the order is closed due to insufficient funds with no execution
  * CLEARED: Order fully executed
  * CLOSED: Order partially executed but closed due to insufficient funds
  * EXPIRED: Order not executed or partially executed upon expiry
