29c274c4806fcf6fe167614d85967d1d1bf3ec2a
essteer <essteer@pm.me>
2024-09-06 13:48:25 +0800

    refactor: Amend FxController to receive currency ID instead of currency obj, and add relevant methods to FxService and FxRepo to retrieve FxRate obj based on that currency ID - all tests checked and passing

--- src/main/java/com/fdmgroup/forex/repos/FxRateRepo.java
+++ src/main/java/com/fdmgroup/forex/repos/FxRateRepo.java
@@ -6,12 +6,11 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fdmgroup.forex.models.Currency;
 import com.fdmgroup.forex.models.FxRate;
 
 @Repository
 public interface FxRateRepo extends JpaRepository<FxRate, UUID> {
 
-    Optional<FxRate> findByCurrency(Currency currency);
+    Optional<FxRate> findByCurrency_CurrencyId(String id);
 
 }
