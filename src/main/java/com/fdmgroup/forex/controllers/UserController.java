forex-agile/backend

commit 1fc889bc97dda5cb987d716f23c9ccb683e3de78

feat: by default ignore user sensitive fields
And remove UserPublicInfoDTO and GetPortfolioDTO because now by default
already ignore sensitive fields from User object, using @JsonIgnore

src/main/java/com/fdmgroup/forex/controllers/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fdmgroup.forex.models.User;
 import com.fdmgroup.forex.models.dto.UserDetailsDTO;
-import com.fdmgroup.forex.models.dto.UserPublicInfoDTO;
 import com.fdmgroup.forex.services.UserService;
 
 import jakarta.validation.Valid;
@@ -26,9 +25,8 @@ public class UserController {
 	private UserService userService;
 
 	@GetMapping("/user/{id}")
-	public UserPublicInfoDTO getUserPublicInfo(@PathVariable UUID id) {
-		User user = userService.findUserById(id);
-		return new UserPublicInfoDTO(user);
+	public User getUserPublicInfo(@PathVariable UUID id) {
+		return userService.findUserById(id);
 	}
 
 	@PostMapping("/register")

src/main/java/com/fdmgroup/forex/models/User.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -37,31 +39,38 @@ public class User {
 	@NotNull(message = "Email must be present.")
 	@Email(message = "Email must be a valid email address.")
 	@Column(unique = true, nullable = false)
+	@JsonIgnore
 	private String email;
 
 	@NotNull(message = "Password must be present.")
 	@NotBlank(message = "Password cannot be blank or contain only whitespace characters.")
 	@Column(nullable = false)
+	@JsonIgnore
 	private String password;
 
 	@ManyToOne
 	@JoinColumn(name = "FK_Preferred_Currency_Code")
+	@JsonIgnore
 	private Currency preferredCurrency;
 
+	@JsonIgnore
 	private String bankAccount;
 
 	@ManyToOne
 	@JoinColumn(name = "FK_Role_Id")
+	@JsonIgnore
 	private Role role;
 
 	@Column(nullable = false, updatable = false)
 	@CreationTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
+	@JsonIgnore
 	private Date createdDate;
 
 	@Column(nullable = false, updatable = false)
 	@UpdateTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
+	@JsonIgnore
 	private Date modifiedDate;
 
 	public User() {

src/main/java/com/fdmgroup/forex/controllers/PortfolioController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdmgroup.forex.models.Portfolio;
-import com.fdmgroup.forex.models.dto.GetPortfolioDTO;
 import com.fdmgroup.forex.services.PortfolioService;
 
 @RestController
@@ -26,15 +25,15 @@ public class PortfolioController {
 	}
 
 	@GetMapping("/username/{username}")
-	public ResponseEntity<GetPortfolioDTO> getPortfolioByUsername(@PathVariable String username) {
+	public ResponseEntity<Portfolio> getPortfolioByUsername(@PathVariable String username) {
 		Portfolio portfolio = portfolioService.findPortfolioByUsername(username);
-		return ResponseEntity.ok(new GetPortfolioDTO(portfolio));
+		return ResponseEntity.ok(portfolio);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<GetPortfolioDTO> getPortfolio(@PathVariable UUID id) {
+	public ResponseEntity<Portfolio> getPortfolio(@PathVariable UUID id) {
 		Portfolio portfolio = portfolioService.findPortfolioById(id);
-		return ResponseEntity.ok(new GetPortfolioDTO(portfolio));
+		return ResponseEntity.ok(portfolio);
 	}
 
 }

src/main/java/com/fdmgroup/forex/models/dto/GetPortfolioDTO.java
@@ -1,38 +1,0 @@
-package com.fdmgroup.forex.models.dto;
-
-import java.util.List;
-import java.util.UUID;
-
-import com.fdmgroup.forex.models.Asset;
-import com.fdmgroup.forex.models.Portfolio;
-
-/**
- * GetPortfolioDTO
- */
-public class GetPortfolioDTO {
-
-	private final UUID id;
-
-	private final UserPublicInfoDTO user;
-
-	private final List<Asset> assets;
-
-	public GetPortfolioDTO(Portfolio portfolio) {
-		this.id = portfolio.getId();
-		this.user = new UserPublicInfoDTO(portfolio.getUser());
-		this.assets = portfolio.getAssets();
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public UserPublicInfoDTO getUser() {
-		return user;
-	}
-
-	public List<Asset> getAssets() {
-		return assets;
-	}
-
-}

src/main/java/com/fdmgroup/forex/models/dto/UserPublicInfoDTO.java
@@ -1,28 +1,0 @@
-package com.fdmgroup.forex.models.dto;
-
-import java.util.UUID;
-
-import com.fdmgroup.forex.models.User;
-
-/**
- * Data transfer object for user public information
- */
-public class UserPublicInfoDTO {
-
-	private UUID id;
-	private String username;
-
-	public UserPublicInfoDTO(User user) {
-		this.id = user.getId();
-		this.username = user.getUsername();
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-}

commit 817a8e3fb81f30af8a5d1d256ba89433bbe2a36d

fix: user registration
Adding @JsonIgnore to fields in previous commit 1fc889b broke user
registration because the annotation also prevents deserialization.

src/main/java/com/fdmgroup/forex/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fdmgroup.forex.models.User;
+import com.fdmgroup.forex.models.dto.RegisterUserDTO;
 import com.fdmgroup.forex.models.dto.UserDetailsDTO;
 import com.fdmgroup.forex.services.UserService;
 
@@ -30,8 +31,8 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public ResponseEntity<UserDetailsDTO> register(@Valid @RequestBody User user) {
-		user = userService.createUser(user);
+	public ResponseEntity<UserDetailsDTO> register(@Valid @RequestBody RegisterUserDTO registerUserDTO) {
+		User user = userService.createUser(registerUserDTO);
 		UserDetailsDTO userDetailsDTO = new UserDetailsDTO(user);
 		URI location = ServletUriComponentsBuilder
 				.fromCurrentRequest()

src/main/java/com/fdmgroup/forex/models/User.java
@@ -17,9 +17,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 /**
  * Model a user account on the Forex platform
@@ -31,19 +28,13 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 
-	@NotNull(message = "Username must be present.")
-	@NotBlank(message = "Username cannot be blank or contain only whitespace characters.")
 	@Column(unique = true, nullable = false)
 	private String username;
 
-	@NotNull(message = "Email must be present.")
-	@Email(message = "Email must be a valid email address.")
 	@Column(unique = true, nullable = false)
 	@JsonIgnore
 	private String email;
 
-	@NotNull(message = "Password must be present.")
-	@NotBlank(message = "Password cannot be blank or contain only whitespace characters.")
 	@Column(nullable = false)
 	@JsonIgnore
 	private String password;

src/main/java/com/fdmgroup/forex/models/dto/RegisterUserDTO.java
@@ -1,0 +1,39 @@
+package com.fdmgroup.forex.models.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Model a user account on the Forex platform
+ */
+public class RegisterUserDTO {
+
+	@NotNull(message = "Username must be present.")
+	@NotBlank(message = "Username cannot be blank or contain only whitespace characters.")
+	private String username;
+
+	@NotNull(message = "Email must be present.")
+	@Email(message = "Email must be a valid email address.")
+	private String email;
+
+	@NotNull(message = "Password must be present.")
+	@NotBlank(message = "Password cannot be blank or contain only whitespace characters.")
+	private String password;
+
+	public RegisterUserDTO() {
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}

src/main/java/com/fdmgroup/forex/services/UserService.java
@@ -12,6 +12,7 @@ import com.fdmgroup.forex.exceptions.ResourceConflictException;
 import com.fdmgroup.forex.models.Portfolio;
 import com.fdmgroup.forex.models.Role;
 import com.fdmgroup.forex.models.User;
+import com.fdmgroup.forex.models.dto.RegisterUserDTO;
 import com.fdmgroup.forex.repos.PortfolioRepo;
 import com.fdmgroup.forex.repos.RoleRepo;
 import com.fdmgroup.forex.repos.UserRepo;
@@ -44,16 +45,19 @@ public class UserService {
 	}
 
 	@Transactional
-	public User createUser(User user) {
-		if (hasConflictUsername(user.getUsername()))
+	public User createUser(RegisterUserDTO registerUserDTO) {
+		if (hasConflictUsername(registerUserDTO.getUsername()))
 			throw new ResourceConflictException("Username already exists.", "username");
-		if (hasConflictEmail(user.getEmail()))
+		if (hasConflictEmail(registerUserDTO.getEmail()))
 			throw new ResourceConflictException("Email already exists.", "email");
 
 		Role role = roleRepo.findByRole(defaultRole).orElseThrow(
 				() -> new InternalServerErrorException("Could not assign role to new user"));
 
-		user.setPassword(pwdEncoder.encode(user.getPassword()));
+		User user = new User();
+		user.setUsername(registerUserDTO.getUsername());
+		user.setEmail(registerUserDTO.getEmail());
+		user.setPassword(pwdEncoder.encode(registerUserDTO.getPassword()));
 		user.setRole(role);
 
 		user = userRepo.save(user);