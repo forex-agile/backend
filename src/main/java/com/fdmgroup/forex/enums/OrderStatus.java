593c59162fe1595128ade621afe5f4076d010789
essteer <essteer@pm.me>
2024-09-06 16:54:09 +0800

    chore: Add javadocs to describe enum states for OrderStatus

--- src/main/java/com/fdmgroup/forex/enums/OrderStatus.java
+++ src/main/java/com/fdmgroup/forex/enums/OrderStatus.java
@@ -1,5 +1,13 @@
 package com.fdmgroup.forex.enums;
 
+/**
+ * Denotes the state of the order as follows:
+ * ACTIVE: Order is live and open to matches
+ * CANCELLED: Order cancelled by the user
+ * CLEARED: Order fully executed
+ * CLOSED: Order partially executed but closed due to insufficient funds
+ * EXPIRED: Order not executed or partially executed upon expiry
+ */
 public enum OrderStatus {
     ACTIVE, CANCELLED, CLEARED, CLOSED, EXPIRED
 }
