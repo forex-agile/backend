3d5c7bd5344db84bda23b2221ada5d8b3df0a853
Dexter Lui <dexlui@pm.me>
2024-09-08 15:48:31 +0800

    feat(PortfolioController): add access control

--- src/main/java/com/fdmgroup/forex/controllers/PortfolioController.java
+++ src/main/java/com/fdmgroup/forex/controllers/PortfolioController.java
@@ -3,6 +3,8 @@ package com.fdmgroup.forex.controllers;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,12 +27,14 @@ public class PortfolioController {
 	}
 
 	@GetMapping("/user/{id}")
+	@PreAuthorize("#id.toString() == authentication.principal.claims['userId']")
 	public ResponseEntity<Portfolio> getPortfolioByUserId(@PathVariable UUID id) {
 		Portfolio portfolio = portfolioService.findPortfolioByUserId(id);
 		return ResponseEntity.ok(portfolio);
 	}
 
 	@GetMapping("/{id}")
+	@PostAuthorize("returnObject.getBody().user.id == authentication.principal.claims['userId']")
 	public ResponseEntity<Portfolio> getPortfolio(@PathVariable UUID id) {
 		Portfolio portfolio = portfolioService.findPortfolioById(id);
 		return ResponseEntity.ok(portfolio);
