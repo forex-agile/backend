64c010d25608da77d9107a432c86ff8930e515d1
DexQ <dexlui@pm.me>
2024-09-09 11:26:04 +0800

    feat: respond with User object on login success (#9)

--- src/main/java/com/fdmgroup/forex/security/AuthController.java
+++ src/main/java/com/fdmgroup/forex/security/AuthController.java
@@ -8,28 +8,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fdmgroup.forex.exceptions.RecordNotFoundException;
+import com.fdmgroup.forex.models.User;
+import com.fdmgroup.forex.models.dto.LoginResponseDTO;
+import com.fdmgroup.forex.repos.UserRepo;
+
 @RestController
 @RequestMapping("/api/v1")
 public class AuthController {
 	private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
 
 	private final TokenService tokenService;
+	private final UserRepo userRepo;
 
-	public AuthController(TokenService tokenService) {
+	public AuthController(TokenService tokenService, UserRepo userRepo) {
 		this.tokenService = tokenService;
+		this.userRepo = userRepo;
 	}
 
 	@PostMapping("/login")
-	public String token(Authentication authentication) throws BadRequestException {
+	public LoginResponseDTO token(Authentication authentication) throws BadRequestException {
 		if (authentication == null) {
 			LOG.debug("Token requested for an anonymous user");
 			throw new BadRequestException("Authorization header is missing or invalid");
 		}
 
+		User user = userRepo.findByUsername(authentication.getName()).orElseThrow(
+				() -> new RecordNotFoundException("User not found"));
+
 		LOG.debug("Token requested for user: '{}'", authentication.getName());
 		String token = tokenService.generateToken(authentication);
 		LOG.debug("Token granted for user '{}': {}", authentication.getName(), token);
-		return token;
+
+		return new LoginResponseDTO(user, token);
 	}
 
 }
